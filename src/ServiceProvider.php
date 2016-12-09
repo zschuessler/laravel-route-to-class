9c2a8c134315ad9f98454193f7b9a7c5c55defb0
Zachary Schuessler <zlschuessler@gmail.com>
2016-12-09 13:38:55 -0700

    add view variable that holds all route info as a css class

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Zschuessler\RouteToClass;
 
+
 use Illuminate\Support\Facades\View;
 
 class ServiceProvider extends \Illuminate\Support\ServiceProvider
@@ -13,7 +14,22 @@ class ServiceProvider extends \Illuminate\Support\ServiceProvider
      */
     public function boot()
     {
-        View::composer('*', function ($view) {
+        View::composer('*', function (\Illuminate\View\View $view) {
+            $route = request()->route()->getPath();
+
+            // Remove route parameters. product_id is removed here: `controller/product/{product_id}`
+            $clean = preg_replace("/\{([^}]+)\}/", '', $route);
+
+            // Remove characters that aren't alpha, numeric, or dashes
+            $clean = preg_replace("/[^a-zA-Z0-9\/_|+ -]/", '', $clean);
+
+            // Remove any double dashes from replace functions. eg: `product--name` should be `product-name`
+            $clean = strtolower(trim($clean, '-'));
+
+            // Replace special characters with a dash
+            $clean = preg_replace("/[\/_|+ -]+/", '-', $clean);
+
+            View::share('route_body_classes', $clean);
 
         });
     }
